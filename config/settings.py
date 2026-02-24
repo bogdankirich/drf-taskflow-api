f0d6a01f75c73b965c5336bb2597547b367866a1
Bogdan <bogdanlirichenko57@gmail.com>
2026-02-24 17:09:17 +0000

    Feat: setup Docker and PostgreSQL for local development

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,8 +81,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "taskflow_db"),
+        "USER": os.environ.get("DB_USER", "taskflow_user"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "supersecretpassword"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
